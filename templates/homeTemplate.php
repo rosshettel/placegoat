36c39d5ed8816ffddb939ce3f19d32843d6fc636
Ross Hettel <rosshettel@gmail.com>
2013-03-06 12:18:58 -0600

    first daft of homepage

--- templates/homeTemplate.php
+++ templates/homeTemplate.php
@@ -1,3 +1,28 @@
-<h1>PlaceGOAT</h1>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>PlaceGoat</title>
+		<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+		<style>
+			.container { margin-top: 50px; }
+		</style>
+	</head>
+</html>
 
-<p>Tired of all those placeholder services that want to pe</p>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="span6">
+				<h1>PlaceGOAT</h1>
+
+				<p>Tired of all these placeholder services that just cater to superficial celebrity worship? Missing farm animals in your designs? Have an unhealthy love of yelling goats?</p>
+				<p><strong>You are welcome here, friend.</strong></p>
+			</div>
+
+			<div class="span6">
+				<iframe width="400" height="225" src="http://www.youtube.com/embed/PpccpglnNf0" frameborder="0" allowfullscreen></iframe>
+			</div>
+		</div>
+		
+	</div>
+</body>
