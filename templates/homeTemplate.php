e6cfaf66461a8e1376a99f7302246558bf547277
Ross Hettel <rosshettel@gmail.com>
2013-03-07 00:02:51 -0600

    added google analytics

--- templates/homeTemplate.php
+++ templates/homeTemplate.php
@@ -3,6 +3,18 @@
 	<head>
 		<title>PlaceGoat</title>
 		<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+		<script type="text/javascript">
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-39096722-1']);
+		  _gaq.push(['_trackPageview']);
+
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+
+		</script>
 		<style>
 			.container { margin-top: 50px; color: white;}
 			body { background-color: #526F35; }
