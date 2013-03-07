7ceae139838277e99a79b3437fc7edc579105f1f
Ross Hettel <rosshettel@gmail.com>
2013-03-07 00:27:36 -0600

    added github ribbon

--- templates/homeTemplate.php
+++ templates/homeTemplate.php
@@ -24,6 +24,7 @@
 </html>
 
 <body>
+	<a href="https://github.com/rosshettel/placegoat"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png" alt="Fork me on GitHub"></a>
 	<div class="container">
 		<div class="row">
 			<div class="span6">
