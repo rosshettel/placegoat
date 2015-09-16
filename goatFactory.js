6ee8dd30b9e6d187a088bce72e5d4c6e4a47e359
Ross Hettel <rhettel@highground.com>
2015-09-16 15:22:31 -0500

    got twitter count working

--- goatFactory.js
+++ goatFactory.js
@@ -1,12 +1,20 @@
 var GoatFactory = function () {
     var fs = require('fs'),
         gm = require('gm').subClass({imageMagick: true}),
-        jsonFile = require('jsonfile'),
+        Twitter = require('twitter'),
+        twitter = new Twitter({
+            consumer_key: process.env.TWITTER_CONSUMER_KEY,
+            consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+            access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+            access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+        }),
+        jsonfile = require('jsonfile'),
         self = this;
 
     const GOAT_DIR = __dirname + '/public_html/goats/';
     const GOATSE = __dirname + '/public_html/goatsee.jpg';
-    const COUNT_FILE = __dirname + '/public_html/count.json'
+    const COUNT_FILE = __dirname + '/public_html/count.json';
+    const TWEET_REGEX = /([0-9]+)(?= goats served!)/;
 
     this.grabAGoat = function (params, callback) {
         var goat = params.goatse ? GOATSE : self.getRandomGoat();
@@ -23,19 +31,33 @@ var GoatFactory = function () {
     };
 
     this.updateGoatsServedCount = function () {
-        jsonFile.readFile(COUNT_FILE, function (err, file) {
+        twitter.get('statuses/user_timeline', {screen_name: 'placegoats'}, function (err, tweets) {
             if (err) {
-                console.log('Error reading file:', err);
-                file = { count: 6113 };
+                console.log('Error getting tweets:', err);
+                return;
             }
 
-            file.count++;
-            process.env.GOAT_COUNT = file.count;
-            console.log('updated count', file);
+            //find last tweet mentioning "goats served!"
+            var countTweets = tweets.filter(function (tweet) {
+                    return TWEET_REGEX.exec(tweet.text);
+                }),
+                lastCount = TWEET_REGEX.exec(countTweets[0].text)[0];
 
-            jsonFile.writeFile(COUNT_FILE, file, function (err) {
+            //that's the count, increment it
+            lastCount++;
+
+            //write it to the json file too, that works pretty well for the frontend
+            jsonfile.writeFile(COUNT_FILE, {count: lastCount}, function (err) {
                 if (err) {
-                    console.log('Error writing file:', err);
+                    console.log('Error writing count file:', err);
+                }
+            });
+
+            //send out a tweet with the new count
+            var tweet = {status: "I just served someone a #goat! That's " + lastCount + " goats served!"};
+            twitter.post('statuses/update', tweet, function (err, tweet, response) {
+                if (err) {
+                    console.log('Error posting tweet:', err);
                 }
             });
         });
