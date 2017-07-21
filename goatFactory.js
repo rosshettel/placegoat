a87ef65ef2853a7ed6beae42026ed4ebd94f2e66
Ross Hettel <ross@het.tel>
2017-07-21 11:16:28 -0500

    remove twitter, save goat count to file

--- goatFactory.js
+++ goatFactory.js
@@ -1,21 +1,13 @@
 var GoatFactory = function () {
     var fs = require('fs'),
         gm = require('gm').subClass({imageMagick: true}),
-        Twitter = require('twitter'),
-        twitter = new Twitter({
-            consumer_key: process.env.TWITTER_CONSUMER_KEY,
-            consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-            access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-            access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-        }),
         logger = require('./logger'),
         self = this,
         cachedGoatCount;
 
     const GOAT_DIR = __dirname + '/public_html/goats/';
     const GOATSE = __dirname + '/public_html/goatsee.jpg';
-    const COUNT_FILE = __dirname + '/public_html/count.json';
-    const TWEET_REGEX = /([0-9]+)(?= goats served!)/;
+    const GOAT_COUNT = '/srv/placegoat/goatcount.txt';
 
     this.grabAGoat = function (params, callback) {
         var goat = params.goatse ? GOATSE : self.getRandomGoat();
@@ -33,20 +25,14 @@ var GoatFactory = function () {
         if (cachedGoatCount) {
             callback(null, cachedGoatCount);
         } else {
-            twitter.get('statuses/user_timeline', {screen_name: 'placegoats'}, function (err, tweets) {
+            fs.readFile(GOAT_COUNT, function (err, data) {
                 if (err) {
-                    logger.error('Error getting tweets:', err);
+                    logger.error('Error getting goat count', err);
                     return;
                 }
-
-                //find last tweet mentioning "goats served!"
-                var countTweets = tweets.filter(function (tweet) {
-                        return TWEET_REGEX.exec(tweet.text);
-                    });
-
-                cachedGoatCount = TWEET_REGEX.exec(countTweets[0].text)[0];
+                cachedGoatCount = parseInt(data, 10);
                 callback(null, cachedGoatCount);
-            })
+            });
         }
     };
 
@@ -59,13 +45,12 @@ var GoatFactory = function () {
 
             //that's the count, increment it
             lastCount++;
-            cachedGoatCount++;
+            cachedGoatCount = lastCount;
             
-            //send out a tweet with the new count
-            var tweet = {status: "I just served someone a #goat! That's " + lastCount + " goats served!"};
-            twitter.post('statuses/update', tweet, function (err, tweet, response) {
+            //write the new count to file
+            fs.writeFile(GOAT_COUNT, cachedGoatCount, function (err) {
                 if (err) {
-                    logger.error('Error posting tweet:', err);
+                    logger.error('Error writing goat count', err);
                 }
             });
         });
